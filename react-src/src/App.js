d560d1d171739855a25014d1aff44290caec8fc2
Ram Vennam <rvennam987@gmail.com>
2017-03-24 18:11:16 -0500

    scrolling banner

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -4,6 +4,7 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import BlogCard from './BlogCard.js'
 import ClickableBlogCard from './ClickableBlogCard.js'
 var blogData = require('./blogData.json');
+var Slider = require('react-slick');
 
 class App extends Component {
 
@@ -16,14 +17,28 @@ class App extends Component {
     return bc;
   }
   render() {
+    var settings = {
+      dots: true,
+      arrows: false,
+      infinite: true,
+      speed: 500,
+      autoplay: true,
+    };
 
     return (
       <MuiThemeProvider >
         <div className="App">
+          <Slider {...settings}>
+            {['rvennam_banner2.jpg', 'rvennam_banner.jpeg'].map((imgSrc) => <div className="bannerContainer">
+              <img className = "bannerSlider" src = {imgSrc} />
+            </div>)
+            }
+          </Slider>
           <div className="App-header">
             <h1>Ram Vennam</h1>
             <h3>Bluemix Developer Advocate</h3>
           </div>
+
           <div className="CardContainer">
           {blogData.map(this.selectBlog)}
         </div>
