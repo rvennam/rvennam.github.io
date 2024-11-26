8e1aea70caa8c14f2f5ff876bba61c4eb78a882f
Ram Vennam <ram.vennam@solo.io>
2024-11-25 20:38:52 -0500

    2024 updates

--- react-src/src/App.js
+++ react-src/src/App.js
@@ -41,7 +41,7 @@ export class App extends Component {
             <h1>Ram Vennam</h1>
 
           </div>
-          <p className="aboutme">Solutions Engineer with 11+ years of expertise in Kubernetes, Istio Service Mesh, and cloud-native technologies, specializing in designing and deploying large-scale, resilient, distributed architectures. Proven ability to lead technical sales engagements, delivering tailored solutions to customer challenges, and collaborating with cross-functional teams to drive innovative product development and customer success. Passionate advocate for CNCF projects, leveraging strategic speaking engagements and thought leadership to accelerate cloud-native adoption and foster industry-wide innovation.</p>
+          <p className="aboutme">Solutions-driven engineer with 11+ years of expertise in Kubernetes, Istio Service Mesh, API gateways, and cloud-native technologies, specializing in designing and deploying large-scale, resilient, distributed architectures. Proven ability to lead technical sales engagements, deliver tailored solutions to complex customer challenges, and collaborate with cross-functional teams to drive innovative product development and customer success. Extensive experience in architecting and optimizing API gateway solutions to enable secure, scalable, and efficient service integrations. Skilled in aligning technical capabilities with business objectives, defining product strategies, and driving the adoption of cloud-native technologies. Passionate advocate for CNCF projects and industry innovation, leveraging thought leadership and speaking engagements to accelerate cloud-native adoption and influence technology trends.</p>
 
           <div className="CardContainer">
           {data.blogs.map(this.selectBlog)}
